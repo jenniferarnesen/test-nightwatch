ede48522c47efa392020aa913f8d63e5fd51d53b
Jen Jones Arnesen <jarnesen@baosystems.com>
2017-09-18 08:09:00 +0200

    Do not run loginPage test for now

--- features/step_definitions/dashboard.js
+++ features/step_definitions/dashboard.js
@@ -2,13 +2,19 @@ const { client } = require('nightwatch-cucumber');
 const { defineSupportCode } = require('cucumber');
 
 defineSupportCode(({Given, Then, BeforeAll, AfterAll}) => {
-    BeforeAll({tags: "@dashboard"}, () => client.login(client.launchUrl, 'admin', 'district'));
+    // BeforeAll({tags: '@dashboard'}, () => client.login(client.launchUrl, 'admin', 'district'));
 
-    Given(/^I am logged in$/, () => client.waitForElementVisible('body', 2000));
-
-    Then(/^the dashboard page should be loaded$/, () => {
-        return client.assert.elementPresent('#mainPage');
+    Given(/^I am logged in$/, () => {
+        client
+            .login(client.launchUrl, 'admin', 'district')
+            .waitForElementVisible('body', 2000)
     });
 
-    AfterAll({tags: "@dashboard"}, () => client.logout(client.launchUrl));
+    Then(/^the dashboard page should be loaded$/, () => {
+        return client
+            .assert.elementPresent('#mainPage')
+            .logout(client.launchUrl);
+    });
+
+    // AfterAll({tags: "@dashboard"}, () => client.logout(client.launchUrl));
 });
